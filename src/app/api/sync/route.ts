6c63fd6e4034f5999f05ab5d7ac39867b9d83e19
Muhammad Naufan <muhammadnaufan719@yahoo.com>
2026-03-04 04:55:12 +0700

    refactor: optimize sync data country from api to database.

--- src/app/api/sync/route.ts
+++ src/app/api/sync/route.ts
@@ -11,48 +11,53 @@ interface CountryApiData {
 }
 
 export const POST = async () => {
+  const CHUNK_SIZE = 50;
+
   try {
     const res = await fetch('https://restcountries.com/v3.1/all?fields=name,capital,region,flags,population,cca3');
     if (!res.ok) throw new Error('Failed to fetch from external API');
     
     const countries: CountryApiData[] = await res.json();
-        
-    await prisma.$transaction(
-      countries.map((c) => 
-        prisma.country.upsert({
-          where: { cca3: c.cca3 },
-          update: {
-            name: c.name.common,
-            capital: c.capital?.[0] || null,
-            region: c.region,
-            flagUrl: c.flags.png,
-            population: c.population,
-          }, 
-          create: {
-            cca3: c.cca3,
-            name: c.name.common,
-            capital: c.capital?.[0] || null,
-            region: c.region,
-            flagUrl: c.flags.png,
-            population: c.population,
-          },
-        })
-      )
-    );
+
+    for (let i = 0; i < countries.length; i += CHUNK_SIZE) {
+      const chunk = countries.slice(i, i + CHUNK_SIZE);
+      await prisma.$transaction(
+        chunk.map((c) => 
+          prisma.country.upsert({
+            where: { cca3: c.cca3 },
+            update: {
+              name: c.name.common,
+              capital: c.capital?.[0] || null,
+              region: c.region,
+              flagUrl: c.flags.png,
+              population: c.population,
+            }, 
+            create: {
+              cca3: c.cca3,
+              name: c.name.common,
+              capital: c.capital?.[0] || null,
+              region: c.region,
+              flagUrl: c.flags.png,
+              population: c.population,
+            },
+          })
+        )
+      );
+
+    }
 
     await prisma.syncLog.create({
       data: {
         count: countries.length,
       }
     });
-
-    return NextResponse.json({ message: 'Synced successfully', count: countries.length });
+    
+    return NextResponse.json({ 
+      message: 'Synced successfully', 
+    });
   } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error('Sync Error:', error.message);
-      return NextResponse.json({ error: error.message }, { status: 500 });
-    }
-    console.error('Unknown Sync Error:', error);
-    return NextResponse.json({ error: 'An unknown error occurred' }, { status: 500 });
+    const message = error instanceof Error ? error.message : 'An unknown error occurred';
+    console.error('❌ Sync Error:', message);
+    return NextResponse.json({ error: message }, { status: 500 });
   }
 }
